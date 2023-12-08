812626f5b9bd4e04da76ee17bf46e87209d6ffd5
Daniel Yadghar <daniel072.dy@gmail.com>
2023-12-08 20:47:56 +0330

    Style searchBar useing css module

--- Contact-Manager/src/Components/SearchBar.tsx
+++ Contact-Manager/src/Components/SearchBar.tsx
@@ -1,18 +1,19 @@
 import { BsSearch } from "react-icons/bs";
 import { useSearchParams } from "react-router-dom";
+import styles from "../Styles/searchBar.module.css";
 
 const SearchBar = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   return (
-    <div className="input_container">
-      <button className="search_button" type="submit">
+    <div className={styles.searchContainer}>
+      <button className={styles.searchButton} type="submit">
         <BsSearch size="30px" color="#9DB2BF" />
       </button>
       <input
         value={searchParams.get("filter") || ""}
         onChange={(event) => setSearchParams({ filter: event.target.value })}
-        className="contact_search"
+        className={styles.contactSearch}
         type="search"
         placeholder="Contact Search"
       />
