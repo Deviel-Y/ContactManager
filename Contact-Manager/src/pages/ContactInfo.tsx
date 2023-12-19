ecfc23e584779e3d629016ff401e3171ff0f7e20
Daniel Yadghar <daniel072.dy@gmail.com>
2023-12-19 08:33:14 +0330

    ContactInfo : Show nothing on empty string

--- Contact-Manager/src/pages/ContactInfo.tsx
+++ Contact-Manager/src/pages/ContactInfo.tsx
@@ -11,28 +11,32 @@ const ContactInfo = () => {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.card}>
-        <div className={styles.photoContainer}>
-          <img
-            className={styles.contactPhoto}
-            src={contact?.photo}
-            alt={`contact name : ${contact?.fullname}`}
-          />
-        </div>
-        <h2 className={styles.contactName}>{contact?.fullname}</h2>
-        <h3 className={styles.contactJob}>{contact?.job}</h3>
+        {contact?.photo && (
+          <div className={styles.photoContainer}>
+            <img
+              className={styles.contactPhoto}
+              src={contact?.photo}
+              alt={`contact name : ${contact?.fullname}`}
+            />
+          </div>
+        )}
+        {contact?.fullname && (
+          <h2 className={styles.contactName}>{contact?.fullname}</h2>
+        )}
+        {contact?.job && <h3 className={styles.contactJob}>{contact?.job}</h3>}
         <ul className={styles.contactInfo}>
-          <li className={styles.contactInfoItem}>
-            <span className={styles.contactInfoItemSpan}>Phone Number :</span>
-            {contact?.mobile}
-          </li>
-          <li className={styles.contactInfoItem}>
-            <span className={styles.contactInfoItemSpan}>Email Address :</span>
-            {contact?.email}
-          </li>
-          <li className={styles.contactInfoItem}>
-            <span className={styles.contactInfoItemSpan}>Group :</span>
-            {contact?.group}
-          </li>
+          {contact?.mobile && (
+            <li className={styles.contactInfoItem}>
+              <span className={styles.contactInfoItemSpan}>Phone Number :</span>
+              {contact?.mobile}
+            </li>
+          )}
+          {contact?.group && (
+            <li className={styles.contactInfoItem}>
+              <span className={styles.contactInfoItemSpan}>Group :</span>
+              {contact?.group}
+            </li>
+          )}
         </ul>
         <div className={styles.actionButtons}>
           <button
